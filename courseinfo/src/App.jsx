6ee32d439a79832e6abf842a00229519b82a8f9e
Albert Chore <79362590+achore26@users.noreply.github.com>
2024-05-16 18:02:28 +0300

    Update App.jsx
    Removal of unwanted space

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -23,7 +23,6 @@
       <Part part={props.parts.parts[0].name} exercise={props.parts.parts[0].exercises} />
       <Part part={props.parts.parts[1].name} exercise={props.parts.parts[1].exercises} />
       <Part part={props.parts.parts[2].name} exercise={props.parts.parts[2].exercises} />
- 
     </div>
   )
   }
